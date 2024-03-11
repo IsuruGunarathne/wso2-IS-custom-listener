0c2529ae3908288837b60ef0cbd3db61c76a925d
Isuru <isuru623@gmail.com>
2024-03-11 15:37:36 +0530

    Add sql queries

---
+++ SQLQuerys.sql
@@ -0,0 +1,10 @@
+select * from UM_USER where UM_USER_NAME='tesst';
+select UM_ID from UM_USER where UM_USER_NAME='tesst';
+select UM_USER_ID from UM_USER where UM_USER_NAME='tesst';
+
+select * from UM_USER_ATTRIBUTE where UM_USER_ID = '3';
+
+select * from UM_UUID_DOMAIN_MAPPER where UM_USER_ID='b0741828-c69d-4377-8dde-5b6f38247952';
+
+select * from UM_UUID_DOMAIN_MAPPER;
+select * from UM_USER;
