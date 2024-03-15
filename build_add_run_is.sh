c7d0d8eed43df29a1203464cfe69d8eca5d1e783
Isuru <isuru623@gmail.com>
2024-03-15 12:33:30 +0530

    Update correct copy path

--- build_add_run_is.sh
+++ build_add_run_is.sh
@@ -7,11 +7,11 @@ destination= wso2is-7.0.0/repository/components/dropins
 JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 mvn clean install
 
 # copy to the dropins folder
-rm -rf wso2is-7.0.0/repository/components/dropins/org.wso2.custom.user.operation.event.listener_1.0_SNAPSHOT_1.0.0.jar
-cp target/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar wso2is-7.0.0/repository/components/lib
+rm -rf wso2is-7.0.0/repository/components/dropins/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar
+cp target/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar wso2is-7.0.0/repository/components/dropins
 
 # restart the server
 echo "Restarting the server"
-./wso2is-7.0.0/bin/wso2server.sh -DosgiConsole
+./wso2is-7.0.0/bin/wso2server.sh
 
 
