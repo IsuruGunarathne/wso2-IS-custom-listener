3801998a9248ff671003d048fcc7549c3e751e1f
Isuru <isuru623@gmail.com>
2024-03-15 12:49:26 +0530

    Add script to add libraries to IS

--- build_add_run_is.sh
+++ build_add_run_is.sh
@@ -10,6 +10,10 @@ JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 mvn clean install
 rm -rf wso2is-7.0.0/repository/components/dropins/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar
 cp target/org.wso2.custom.user.operation.event.listener-1.0-SNAPSHOT.jar wso2is-7.0.0/repository/components/dropins
 
+
+# copy libraries from libraries directory to the lib folder
+cp libraries/* wso2is-7.0.0/repository/components/lib
+
 # restart the server
 echo "Restarting the server"
 ./wso2is-7.0.0/bin/wso2server.sh
